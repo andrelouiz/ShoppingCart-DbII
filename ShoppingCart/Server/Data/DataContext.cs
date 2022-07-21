969e6aac0c6a387f9273d2a0034596063814d7aa
andrelouiz <andrelouiz@outlook.com>
2022-07-22 01:01:16 +0200

    errors

---
+++ ShoppingCart/Server/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingCart.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
